91041af64b73f75288fcbc73ceb07f003363461f
Shani.ha <53007257+shhatami@users.noreply.github.com>
2023-02-04 13:57:18 +0330

    Create the required functions.py

---
+++ the required functions.py
@@ -0,0 +1,49 @@
+import numpy as np
+import sklearn.cluster as cl
+import sklearn.metrics as met
+import matplotlib.pyplot as plt
+import sklearn.preprocessing as pp
+
+def get_distance(x1, x2):
+    sum = 0
+    for i in range(len(x1)):
+        sum += (x1[i] - x2[i]) ** 2
+    return np.sqrt(sum)
+
+
+def kmeans(X, k, max_iters):
+  
+    centroids = X[np.random.choice(range(len(X)), k, replace=False)]
+
+    converged = False
+    
+    current_iter = 0
+
+    while (not converged) and (current_iter < max_iters):
+
+        cluster_list = [[] for i in range(len(centroids))]
+
+        for x in X:  # Go through each data point
+            distances_list = []
+            for c in centroids:
+                distances_list.append(get_distance(c, x))
+            cluster_list[int(np.argmin(distances_list))].append(x)
+
+        cluster_list = list((filter(None, cluster_list)))
+
+        prev_centroids = centroids.copy()
+
+        centroids = []
+
+        for j in range(len(cluster_list)):
+            centroids.append(np.mean(cluster_list[j], axis=0))
+
+        pattern = np.abs(np.sum(prev_centroids) - np.sum(centroids))
+
+        print('K-MEANS: ', int(pattern))
+
+        converged = (pattern == 0)
+
+        current_iter += 1
+
+    return np.array(centroids), [np.std(x) for x in cluster_list]
